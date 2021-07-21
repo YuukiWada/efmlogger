2bdf116b57477988c242d26af0111e05c6afaf0d
YuukiWada <yuphys.expressif@gmail.com>
2021-07-21 02:51:35 +0100

    ruby script update

--- scripts/read_mcp.rb
+++ scripts/read_mcp.rb
@@ -1,6 +1,9 @@
 #!/usr/bin/env ruby
 require "pi_piper"
 
+start=Time.now.strftime('%s%L').to_i
+puts "#{(start/1000.0).round(3)}"
+
 PiPiper::Spi.begin do |spi|
   loop do
     first, center, last = spi.write [0b00000001, 0b10000000, 0b00000000]
@@ -8,14 +11,15 @@ PiPiper::Spi.begin do |spi|
     center = center << 8
     adc =  center + last
 
-    sleep 0.001
+    sleep 0.01
     
     first, center, last = spi.write [0b00000001, 0b11000000, 0b00000000]
     center = center & 0b00001111
     center = center << 8
     pps =  center + last
 
-    puts "#{Time.now.strftime('%s%L').to_i/1000.0},#{adc},#{pps}"
-    sleep 0.0485
+    #puts "#{Time.now.strftime('%s%L').to_i/1000.0},#{adc},#{pps}"
+    puts "#{((Time.now.strftime('%s%L').to_i-start)/1000.0).round(3)},#{adc},#{pps}"
+    sleep 0.04
   end
 end
