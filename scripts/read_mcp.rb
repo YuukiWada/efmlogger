43a17a382d0680844dd08a8994b26e8bad237dab
YuukiWada <yuphys.expressif@gmail.com>
2021-10-05 10:34:43 +0900

    Updated read_mcp.rb for MCP3208

--- scripts/read_mcp.rb
+++ scripts/read_mcp.rb
@@ -6,18 +6,23 @@ puts "#{(start/1000.0).round(3)}"
 
 PiPiper::Spi.begin do |spi|
   loop do
-    first, center, last = spi.write [0b00000001, 0b10000000, 0b00000000]
+    first, center, last = spi.write [0b00000110, 0b00000000, 0b00000000]
     center = center & 0b00001111
     center = center << 8
-    adc =  center + last
+    adc0 =  center + last
 
-    first, center, last = spi.write [0b00000001, 0b11000000, 0b00000000]
+    first, center, last = spi.write [0b00000110, 0b01000000, 0b00000000]
+    center = center & 0b00001111
+    center = center << 8
+    adc1 =  center + last
+
+    first, center, last = spi.write [0b00000110, 0b10000000, 0b00000000]
     center = center & 0b00001111
     center = center << 8
     pps =  center + last
 
-    puts "#{((Time.now.strftime('%s%L').to_i-start)/1000.0).round(3)},#{adc},#{pps}"
-    sleep 0.035
+    puts "#{((Time.now.strftime('%s%L').to_i-start)/1000.0).round(3)},#{adc0},#{adc1},#{pps}"
+    sleep 0.03
     
   end
 end
